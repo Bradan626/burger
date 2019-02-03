232703b049337ebc8bf703a1e882b4cb8e8c6231
Bradan626 <bradan626@gmail.com>
2019-02-03 13:12:57 -0500

    plz work

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 // Node Dependency
-var mysql = require('mysql2');
+var mysql = require("mysql2");
 var connection;
 
 // For Heroku Deployment vs. Local MySQL Database
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root', 
-        port: 3306,
-        database: 'burgers_db' 
-    });
+  connection = mysql.createConnection({
+    host: "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "js5no3hpkrya6jln",
+    password: "pnhjzrh30ia2bnfi",
+    port: 3306,
+    database: "cbqj9o2jh5guflf8"
+  });
 }
-
 
 // Export the Connection
 module.exports = connection;
